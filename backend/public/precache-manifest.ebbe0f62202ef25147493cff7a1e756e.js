66071e6dd5c1977b4321f31034158a095522314b
tat.bandelikova <tat.bandelikova@gmail.com>
2022-12-14 20:27:15 +0300

    deploy with public

---
+++ backend/public/precache-manifest.ebbe0f62202ef25147493cff7a1e756e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ffe832ef9282362808a",
+    "url": "/static/css/main.58bf5721.chunk.css"
+  },
+  {
+    "revision": "5ffe832ef9282362808a",
+    "url": "/static/js/main.5ffe832e.chunk.js"
+  },
+  {
+    "revision": "8f1f9bdd8571ce5e7bb7",
+    "url": "/static/js/1.8f1f9bdd.chunk.js"
+  },
+  {
+    "revision": "74b4660fa6c2686dda06",
+    "url": "/static/css/2.e6035d4d.chunk.css"
+  },
+  {
+    "revision": "74b4660fa6c2686dda06",
+    "url": "/static/js/2.74b4660f.chunk.js"
+  },
+  {
+    "revision": "c629b6aa4d20327ca5f7",
+    "url": "/static/js/runtime~main.c629b6aa.js"
+  },
+  {
+    "revision": "b07f5a30c950151dbd8b46c006512fac",
+    "url": "/static/media/Bullion_coins.b07f5a30.png"
+  },
+  {
+    "revision": "df1269d57f0e89db3bfa750cd7d4ff92",
+    "url": "/static/media/Exclusive_coins.df1269d5.png"
+  },
+  {
+    "revision": "bd63c8c1fb75f0714cf3aeabd576498f",
+    "url": "/static/media/Commemorative_coins.bd63c8c1.png"
+  },
+  {
+    "revision": "1cfb9cb42e12ddfb2dc331799f0df09a",
+    "url": "/index.html"
+  }
+];
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const mysql = require('mysql2');
@@ -12,12 +13,13 @@ const DB_CONNECTION = {
 }
 
 var corsOptions = {
-    origin: 'https://catalogue-coins.onrender.com',
+    origin: 'http://localhost:3000',
     optionsSuccessStatus: 200
 }
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname + '/public')));
 
 const connection = mysql.createConnection({
     host: DB_CONNECTION.host,
